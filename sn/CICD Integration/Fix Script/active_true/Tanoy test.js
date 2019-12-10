634a2e1db47c81ab9fef946e35a82fed3555aaa9
System Administrator <admin@example.com>
2019-12-10 14:14:00 +0530

    Changes by 'System Administrator' on 'test12346 2aas 2' -- Build #1

---
+++ sn/CICD Integration/Fix Script/active_true/Tanoy test.js
@@ -0,0 +1,13 @@
+/* 
+ * Application : CICD Integration
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-11-28 07:32:32
+ * Created By  : admin
+ * Updated On  : 2019-12-10 08:42:50
+ * Updated By  : admin
+ * URL         : /sys_script_fix.do?sys_id=ab334283db9540107ad2f36f29961901
+ */
+//gs.log("TestTanoy");
+
+//gs.log("TestTanoy");
+gs.log("Testabcdsc12scxjbdsdjddsdjj312dda329");
