7a4e36b07c2c1d43741e4057e45f0fec845157d0
System Administrator <admin@example.com>
2019-12-10 16:01:55 +0530

    Changes by 'System Administrator' on 'DEVOPStest hhhh' -- Build #1

---
+++ sn/CICD Integration/Fix Script/active_true/DemoCICD.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : CICD Integration
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-11-28 07:07:13
+ * Created By  : admin
+ * Updated On  : 2019-12-10 10:30:30
+ * Updated By  : admin
+ * URL         : /sys_script_fix.do?sys_id=ca7d7d8bdb5540107ad2f36f299619ec
+ */
+gs.log("Test12cxdasdd3");
