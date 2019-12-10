2739fecb68140acd841437ebe87afce9ce7f24b5
System Administrator <admin@example.com>
2019-12-10 17:28:12 +0530

    Changes by 'System Administrator' on 'Adarsh1234' -- Build #1

---
+++ sn/Global/Fix Script/active_true/Adarsh123.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-12-10 11:57:03
+ * Created By  : admin
+ * Updated On  : 2019-12-10 11:57:03
+ * Updated By  : admin
+ * URL         : /sys_script_fix.do?sys_id=91e2380fdba500107ad2f36f29961912
+ */
+gs.log("test12345");
