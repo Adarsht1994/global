e90172ee532aa4c7d4a4cd97a79386cdf69808c2
Adarsh <adarst@deloitte.com>
2019-12-13 01:04:14 -0500

    Changes by 'Adarsh ' on 'New Update Set 1245' -- Build #1

---
+++ sn/Global/Fix Script/active_true/DevopsTest.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-12-11 08:12:26
+ * Created By  : admAdarsh
+ * Updated On  : 2019-12-13 06:03:19
+ * Updated By  : admAdarsh
+ * URL         : /sys_script_fix.do?sys_id=b029c1171b69c41000584225bd4bcbd2
+ */
+gs.log("123");
