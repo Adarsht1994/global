7ab166ee51836805866ed559ce8d6efa0652c967
Adarsh <adarst@deloitte.com>
2019-12-13 04:37:51 -0500

    Changes by 'Adarsh ' on 'DevopsNewtest 5' -- Build #1

--- sn/Global/Fix Script/active_true/Devops.js
+++ sn/Global/Fix Script/active_true/Devops.js
@@ -3,8 +3,8 @@
  * ClassName   : sys_script_fix
  * Created On  : 2019-12-13 07:13:53
  * Created By  : admAdarsh
- * Updated On  : 2019-12-13 07:13:53
+ * Updated On  : 2019-12-13 09:36:40
  * Updated By  : admAdarsh
  * URL         : /sys_script_fix.do?sys_id=b8de4fb71be50c10059a65b82d4bcb86
  */
-gs.log("Abcdefg");
+gs.log("Abcdefghooj");
