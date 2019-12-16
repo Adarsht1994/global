144fa1b1179b624f21ce688771b61c180dc94c06
Adarsh <adarst@deloitte.com>
2019-12-16 08:05:46 -0500

    Changes by 'Adarsh ' on 'Testupdateset 14' -- Build #1

---
+++ sn/Global/Fix Script/active_true/Test1111.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-12-16 13:05:11
+ * Created By  : admAdarsh
+ * Updated On  : 2019-12-16 13:05:11
+ * Updated By  : admAdarsh
+ * URL         : /sys_script_fix.do?sys_id=e10cb33c1b3dc05000584225bd4bcbcc
+ */
+gs.log("test123");
