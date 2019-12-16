2c7d9fd2fa2b22c9c8f5ded41ce9cd2145f213ea
Adarsh <adarst@deloitte.com>
2019-12-16 01:13:05 -0500

    Changes by 'Adarsh ' on 'AdarshNewUpdateset' -- Build #1

---
+++ sn/Global/Fix Script/active_true/AdarshDevops.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-12-16 06:11:14
+ * Created By  : admAdarsh
+ * Updated On  : 2019-12-16 06:11:14
+ * Updated By  : admAdarsh
+ * URL         : /sys_script_fix.do?sys_id=5c4d56fc1bb9c050059a65b82d4bcbcd
+ */
+gs.log("Adarshtest123");
