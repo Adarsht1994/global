2b44f5e2e6e7465231830c89333d752c0bae2018
Adarsh <adarst@deloitte.com>
2019-12-19 12:57:00 -0500

    Changes by 'Adarsh ' on 'Testupdateset 28' -- Build #1

--- sn/Global/Fix Script/active_true/NewFixscript.js
+++ sn/Global/Fix Script/active_true/NewFixscript.js
@@ -3,8 +3,8 @@
  * ClassName   : sys_script_fix
  * Created On  : 2019-12-18 09:01:48
  * Created By  : admAdarsh
- * Updated On  : 2019-12-18 13:53:43
+ * Updated On  : 2019-12-19 17:56:02
  * Updated By  : admAdarsh
  * URL         : /sys_script_fix.do?sys_id=557755a11bb1085000584225bd4bcbad
  */
-gs.log("test45");
+gs.log("test465");
