766b37887add44205482f7e8a1fe8e94671d203f
Adarsh <adarst@deloitte.com>
2019-12-20 09:41:24 -0500

    Changes by 'Adarsh ' on 'GT_BPE_test 10' -- Build #1

---
+++ sn/Global/Fix Script/active_true/NewFixscript9.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-12-20 14:19:34
+ * Created By  : admTrinadh
+ * Updated On  : 2019-12-20 14:40:08
+ * Updated By  : admAdarsh
+ * URL         : /sys_script_fix.do?sys_id=2e73385e1b314c5000584225bd4bcb9c
+ */
+gs.log("test465q");
