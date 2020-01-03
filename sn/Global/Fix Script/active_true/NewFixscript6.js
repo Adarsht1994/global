17366b4c02d615b39cc005bfe324d5b5cba57713
Trinadh <gtrinadh@deloitte.com>
2020-01-03 08:52:00 -0500

    Changes by 'Trinadh  ' on 'GT_BPE_test 6' -- Build #1

---
+++ sn/Global/Fix Script/active_true/NewFixscript6.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2019-12-20 10:38:48
+ * Created By  : admTrinadh
+ * Updated On  : 2019-12-20 10:51:01
+ * Updated By  : admTrinadh
+ * URL         : /sys_script_fix.do?sys_id=e4f0801a1bf90c5000584225bd4bcb42
+ */
+gs.log("test465");
