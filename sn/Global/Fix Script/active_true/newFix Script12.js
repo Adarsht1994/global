bc99effddb9ee2fde739580293e0ae0bdfca884f
Adarsh <adarst@deloitte.com>
2020-01-03 03:43:19 -0500

    Changes by 'Adarsh ' on 'GT_BPE_test 17' -- Build #1

---
+++ sn/Global/Fix Script/active_true/newFix Script12.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2020-01-03 08:40:30
+ * Created By  : admAdarsh
+ * Updated On  : 2020-01-03 08:42:48
+ * Updated By  : admAdarsh
+ * URL         : /sys_script_fix.do?sys_id=f60c272e1b82c8d000584225bd4bcbe0
+ */
+gs.log("newFix Script12 new");
