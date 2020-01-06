75c727a38906f48a9341326bbd427726b61c905d
Ghori Liyakhat <lghori@deloitte.com>
2020-01-06 00:14:50 -0500

    Changes by 'Ghori Liyakhat' on 'LG_COE_Global_Change_V2' -- Build #1

---
+++ sn/Global/Values/wf_activity/158916271b868414059a65b82d4bcb72.js
@@ -0,0 +1,21 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_variable_value
+ * Created On  : 2020-01-06 05:13:30
+ * Created By  : lghori
+ * Updated On  : 2020-01-06 05:13:30
+ * Updated By  : lghori
+ * URL         : /sys_variable_value.do?sys_id=158916271b868414059a65b82d4bcb72
+ */
+// This script needs to set answer to 'yes' or 'no' to indicate the state of the activity.
+//
+// For example,
+//
+//   answer = ifScript();
+//
+//   function ifScript() {
+//      if (condition is true) {
+//         return 'yes';
+//      }
+//      return 'no';
+//   }
