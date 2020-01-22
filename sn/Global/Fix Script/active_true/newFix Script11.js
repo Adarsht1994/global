1d4828d6361383b6710dff1a30dd61f5c13e8251
Adarsh <adarst@deloitte.com>
2020-01-22 07:52:40 -0500

    Changes by 'Adarsh ' on 'NEW Updates in devOps 6' -- Build #2

--- sn/Global/Fix Script/active_true/newFix Script11.js
+++ sn/Global/Fix Script/active_true/newFix Script11.js
@@ -3,8 +3,8 @@
  * ClassName   : sys_script_fix
  * Created On  : 2020-01-03 08:19:52
  * Created By  : admAdarsh
- * Updated On  : 2020-01-22 12:33:19
+ * Updated On  : 2020-01-22 12:48:09
  * Updated By  : admAdarsh
  * URL         : /sys_script_fix.do?sys_id=58572b261b82c8d000584225bd4bcb79
  */
-gs.log("Hiscn jsfijsofsofjosdfjsdjfosdjofnsovndfjivndfji bdghi bdghi bdhibvdfinvdfjinvjidfnvjidfnvjidfnvjidfnvjidfn fjwskd cnsjicnsdjincisdcis");
+gs.log("Hiscn jsfijsofsofjosdfjsdjfosdjofnsovndfjivndfji bdghi bdghi bdhibvdfinvdfjinvjidfnvjiscivvsodfnvjidfnvjidfnvjidfn fjwskd cnsjicnsdjincisdcis");
