722ea39bf662530ec319cc7c804e2dfc305511b6
Adarsh <adarst@deloitte.com>
2020-02-07 01:43:43 -0500

    Changes by 'Adarsh ' on 'New Update Set 5' -- Build #1

---
+++ sn/CICD Integration/Fix Script/active_true/abcdef.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : CICD Integration
+ * ClassName   : sys_script_fix
+ * Created On  : 2020-02-07 06:42:30
+ * Created By  : admAdarsh
+ * Updated On  : 2020-02-07 06:42:57
+ * Updated By  : admAdarsh
+ * URL         : /sys_script_fix.do?sys_id=e0b0e3f11bb2c41400584225bd4bcb20
+ */
+gs.log("test123");
