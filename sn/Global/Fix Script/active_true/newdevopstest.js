d613c7583ed9d067a01062235ebc1998433ee6f1
Adarsh <adarst@deloitte.com>
2020-02-26 02:45:22 -0500

    Changes by 'Adarsh ' on 'abcdtest' -- Build #1

---
+++ sn/Global/Fix Script/active_true/newdevopstest.js
@@ -0,0 +1,10 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_fix
+ * Created On  : 2020-02-26 07:44:43
+ * Created By  : admAdarsh
+ * Updated On  : 2020-02-26 07:44:43
+ * Updated By  : admAdarsh
+ * URL         : /sys_script_fix.do?sys_id=f0256504db13881092b95055ca9619a9
+ */
+gs.log("test233");
