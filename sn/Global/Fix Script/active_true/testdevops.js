20cccd7221b58663c39a171c394554057e55f680
T <adarst@deloitte.com>
2020-03-03 08:34:52 -0500

    Clean-up commit.

--- sn/Global/Fix Script/active_true/testdevops.js
+++ sn/Global/Fix Script/active_true/testdevops.js
@@ -1,10 +1,10 @@
 /* 
  * Application : Global
  * ClassName   : sys_script_fix
- * Created On  : 2020-02-26 07:34:02
+ * Created On  : 2020-03-03 13:34:03
  * Created By  : admAdarsh
- * Updated On  : 2020-02-26 07:34:02
+ * Updated On  : 2020-03-03 13:34:03
  * Updated By  : admAdarsh
- * URL         : /sys_script_fix.do?sys_id=6da26d8cdbdf481092b95055ca96190d
+ * URL         : /sys_script_fix.do?sys_id=bcae29ce1b1b485079c9a9722a4bcbb9
  */
-gs.log("test123");
+gs.log("test");
