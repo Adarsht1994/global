319e2382ca97484a3652f4f437465ff9c87bab11
Adarsh <adarst@deloitte.com>
2020-03-03 04:35:23 -0500

    Changes by 'Adarsh ' on 'NewDevopstest 2223' -- Build #1

--- sn/Global/Fix Script/active_true/newdevopstest.js
+++ sn/Global/Fix Script/active_true/newdevopstest.js
@@ -3,8 +3,8 @@
  * ClassName   : sys_script_fix
  * Created On  : 2020-02-26 07:44:43
  * Created By  : admAdarsh
- * Updated On  : 2020-03-02 13:32:27
+ * Updated On  : 2020-03-03 09:33:10
  * Updated By  : admAdarsh
  * URL         : /sys_script_fix.do?sys_id=f0256504db13881092b95055ca9619a9
  */
-gs.log("test23345678");
+gs.log("test23345900");
