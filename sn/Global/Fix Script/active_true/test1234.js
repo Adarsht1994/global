7330fc486d5cf6f581bffb109dab46bf485e1473
Adarsh <adarst@deloitte.com>
2020-07-08 02:35:28 -0400

    Changes by 'Adarsh ' on 'Servicenow1 2' -- Build #1

--- sn/Global/Fix Script/active_true/test1234.js
+++ sn/Global/Fix Script/active_true/test1234.js
@@ -3,8 +3,8 @@
  * ClassName   : sys_script_fix
  * Created On  : 2020-04-15 09:11:14
  * Created By  : admAdarsh
- * Updated On  : 2020-07-08 06:28:18
+ * Updated On  : 2020-07-08 06:34:44
  * Updated By  : admAdarsh
  * URL         : /sys_script_fix.do?sys_id=38dece731b88dc1079c9a9722a4bcb96
  */
-gs.log("test12345")
+gs.log("test1235")
