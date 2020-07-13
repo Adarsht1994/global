938f2bd1fd89b4b5e8e2b1b8241b20e73d2819ba
Adarsh <adarst@deloitte.com>
2020-07-13 05:23:08 -0400

    Changes by 'Adarsh ' on 'ServicenowTest 4' -- Build #1

--- sn/CICD Integration/Business Rule/sys_update_set/active_true/before/[CICD] Check state changes and run CICD/script.js
+++ sn/CICD Integration/Business Rule/sys_update_set/active_true/before/[CICD] Check state changes and run CICD/script.js
@@ -3,12 +3,12 @@
  * ClassName   : sys_script
  * Created On  : 2019-02-26 13:58:55
  * Created By  : b.moers
- * Updated On  : 2020-01-22 12:52:01
+ * Updated On  : 2020-07-13 09:21:14
  * Updated By  : admAdarsh
  * URL         : /sys_script.do?sys_id=76fd0722dbbbe7409775f6700f9619e3
  */
 (function executeRule(current, previous /*null when async*/) {
 	
-new CiCdRun().sys_update_set_Before_IU(current, previous);
+	new CiCdRun().sys_update_set_Before_IU(current, previous);
 	
 })(current, previous);
