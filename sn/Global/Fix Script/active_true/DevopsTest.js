7737a4de1d4daccfea3a10ad9b86b9c63a97eec0
Adarsh <adarst@deloitte.com>
2020-07-13 01:39:42 -0400

    Changes by 'Adarsh ' on 'ServicenowTest' -- Build #1

--- sn/Global/Fix Script/active_true/DevopsTest.js
+++ sn/Global/Fix Script/active_true/DevopsTest.js
@@ -3,8 +3,9 @@
  * ClassName   : sys_script_fix
  * Created On  : 2020-07-10 12:42:17
  * Created By  : admAdarsh
- * Updated On  : 2020-07-10 12:42:17
+ * Updated On  : 2020-07-13 05:36:21
  * Updated By  : admAdarsh
  * URL         : /sys_script_fix.do?sys_id=28e77de71b39585079c9a9722a4bcb16
  */
-gs.log("Test");
+gs.log("Test123");
+gs.log('TESTDEVOPS1');
